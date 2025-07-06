fe71fce69942dcda4146b0ed9871868cb4be23c7
Igor Kaseko <igorksk@gmail.com>
2025-07-06 18:03:12 +0300

    Init commit

---
+++ 1.1.UniqueSymbols/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Enter string: ");
+
+var str = Console.ReadLine();
+
+// Without using data structures
+
+var hasUnique = true;
+
+for(int i = 0; i < str!.Length; i++)
+{
+    for (int j = i + 1; j < str!.Length; j++)
+    {
+        if (str[i] == str[j])
+        {
+            hasUnique = false; 
+            break;
+        }
+    }
+}
+
+if (hasUnique)
+{
+    Console.WriteLine("Symbols unique");
+}
+else
+{
+    Console.WriteLine("Symbols not unique");
+}
