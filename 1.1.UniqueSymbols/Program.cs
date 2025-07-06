b2e502e60d97070d50a1b3d6ad21b990128d42bc
Igor Kaseko <igorksk@gmail.com>
2025-07-06 21:02:23 +0300

    Add second approach

--- 1.1.UniqueSymbols/Program.cs
+++ 1.1.UniqueSymbols/Program.cs
@@ -1,29 +1,26 @@
 ﻿
+using _1._1.UniqueSymbols;
+
 Console.WriteLine("Enter string: ");
 
-var str = Console.ReadLine();
+var str = Console.ReadLine() ?? throw new NullReferenceException();
 
-// Without using data structures
 
-var hasUnique = true;
-
-for(int i = 0; i < str!.Length; i++)
+if (SymbolsValidators.ValidateWithoutDataStructures(str!))
 {
-    for (int j = i + 1; j < str!.Length; j++)
-    {
-        if (str[i] == str[j])
-        {
-            hasUnique = false; 
-            break;
-        }
-    }
-}
-
-if (hasUnique)
-{
-    Console.WriteLine("Symbols unique");
+    Console.WriteLine("Symbols unique by first check");
 }
 else
 {
-    Console.WriteLine("Symbols not unique");
+    Console.WriteLine("Symbols not unique by first check");
+}
+
+
+if (SymbolsValidators.ValidateUsingHashtable(str!))
+{
+    Console.WriteLine("Symbols unique by second check");
+}
+else
+{
+    Console.WriteLine("Symbols not unique by second check");
 }
---
+++ 1.1.UniqueSymbols/SymbolsValidators.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace _1._1.UniqueSymbols
+{
+    internal class SymbolsValidators
+    {
+        public static bool ValidateWithoutDataStructures(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = i + 1; j < str.Length; j++)
+                {
+                    if (str[i] == str[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool ValidateUsingHashtable(string str)
+        {
+            Hashtable hashtable = [];
+
+            int index = 0;
+
+            foreach (char symbol in str)
+            {
+                if (hashtable.ContainsKey(symbol))
+                {
+                    return false;
+                }
+
+                hashtable.Add(symbol, index);
+
+                index++;
+            }
+
+            return true;
+        }
+    }
+}
