0ae2534f99d7332bc52e336335d17fbc79184196
Igor Kaseko <igorksk@gmail.com>
2025-07-09 08:33:01 +0300

    Permutation strings

---
+++ 1.2.PermutationString/PermutationChecker.cs
@@ -0,0 +1,32 @@
+﻿namespace _1._2.PermutationString
+{
+    internal class PermutationChecker
+    {
+        public static bool CheckIfPermutations(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            int[] letters = new int[128];
+
+            foreach (char c in s1)
+            {
+                letters[c]++;
+            }
+
+            foreach (char c in s2)
+            {
+                letters[c]--;
+
+                if (letters[c] < 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 1.2.PermutationString/Program.cs
@@ -0,0 +1,19 @@
+﻿using _1._2.PermutationString;
+
+Console.WriteLine("Enter string 1: ");
+
+var str1 = Console.ReadLine() ?? throw new NullReferenceException();
+
+Console.WriteLine("Enter string 2: ");
+
+var str2 = Console.ReadLine() ?? throw new NullReferenceException();
+
+
+if (PermutationChecker.CheckIfPermutations(str1, str2))
+{
+    Console.WriteLine("Strings are permutations");
+}
+else
+{
+    Console.WriteLine("Strings are not permutations");
+}
