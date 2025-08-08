da835ce2eadba3ca696b2770b5b8f8774d3bf39c
Igor Kaseko <igorksk@gmail.com>
2025-08-08 07:21:18 +0300

    CountEditDistance

---
+++ 1.5.CountEditDistance/OneEditDistanceChecker.cs
@@ -0,0 +1,76 @@
+﻿namespace _1._5.CountEditDistance
+{
+    internal class OneEditDistanceChecker
+    {
+        public static bool AreOneEditApart(string s1, string s2)
+        {
+            if (s1.Length == s2.Length)
+            {
+                return AreOneEditReplace(s1, s2);
+            }
+            else if (s1.Length + 1 == s2.Length)
+            {
+                return AreOneEditInsert(s1, s2);
+            }
+            else if (s1.Length - 1 == s2.Length)
+            {
+                return AreOneEditInsert(s2, s1);
+            }
+            else
+            {
+                return false; // More than one edit apart
+            }
+        }
+
+        private static bool AreOneEditReplace(string s1, string s2)
+        {
+            const int maxEdits = 1;
+            int editCount = 0;
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] != s2[i])
+                {
+                    editCount++;
+                    if (editCount > maxEdits)
+                    {
+                        return false; // More than one edit
+                    }
+                }
+            }
+            if (editCount == 0)
+            {
+                return false; // No edits, strings are identical
+            }
+            return true; // Exactly one edit or no edits
+        }
+
+        private static bool AreOneEditInsert(string s1, string s2)
+        {
+            const int maxEdits = 1;
+            int editCount = 0;
+
+            int index1 = 0, index2 = 0;
+
+            while (index1 < s1.Length && index2 < s2.Length)
+            {
+                if (s1[index1] != s2[index2])
+                {
+                    editCount++;
+                    if (editCount > maxEdits)
+                    {
+                        return false; // More than one edit
+                    }
+                    index2++; // Move the longer string pointer
+                }
+                else
+                {
+                    index1++;
+                    index2++;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 1.5.CountEditDistance/Program.cs
@@ -0,0 +1,18 @@
+﻿using _1._5.CountEditDistance;
+
+Console.WriteLine("Enter string 1: ");
+
+var str1 = Console.ReadLine() ?? throw new NullReferenceException();
+
+Console.WriteLine("Enter string 2: ");
+
+var str2 = Console.ReadLine() ?? throw new NullReferenceException();
+
+if (OneEditDistanceChecker.AreOneEditApart(str1, str2))
+{
+    Console.WriteLine("Strings are one edit away");
+}
+else
+{
+    Console.WriteLine("Strings are not one edit away");
+}
