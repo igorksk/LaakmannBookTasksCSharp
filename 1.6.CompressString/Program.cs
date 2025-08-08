7f0a4351297c0861b29b8ebccec7f621f3aefd4c
Igor Kaseko <igorksk@gmail.com>
2025-08-08 19:43:29 +0300

    CompressString

---
+++ 1.6.CompressString/Program.cs
@@ -0,0 +1,9 @@
+﻿using _1._6.CompressString;
+
+Console.WriteLine("Enter string: ");
+
+var str = Console.ReadLine() ?? throw new NullReferenceException();
+
+var result = StringCompressor.CompressString(str);
+
+Console.WriteLine(result);
---
+++ 1.6.CompressString/StringCompressor.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace _1._6.CompressString
+{
+    internal class StringCompressor
+    {
+        public static string CompressString(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str; // Return empty string if input is null or empty
+            }
+            int compressedLength = CountCompressedLength(str);
+            StringBuilder compressed = new(compressedLength);
+            char currentChar = str[0];
+            int count = 1;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] == currentChar)
+                {
+                    count++;
+                }
+                else
+                {
+                    compressed.Append(currentChar).Append(count);
+                    currentChar = str[i];
+                    count = 1;
+                }
+            }
+            // Append the last character and its count
+            compressed.Append(currentChar).Append(count);
+            // Return the compressed string if it's shorter than the original, otherwise return the original
+            return compressed.Length < str.Length ? compressed.ToString() : str;
+        }
+
+        private static int CountCompressedLength(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0; // Return 0 if input is null or empty
+            }
+            int compressedLength = 0;
+            char currentChar = str[0];
+            int count = 1;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] == currentChar)
+                {
+                    count++;
+                }
+                else
+                {
+                    compressedLength += 1 + count.ToString().Length; // 1 for the character and length of the count
+                    currentChar = str[i];
+                    count = 1;
+                }
+            }
+            // Add the last character and its count
+            compressedLength += 1 + count.ToString().Length;
+            return compressedLength;
+        }
+    }
+}
