8cea8ad1c6c6a5c7311430cd362602e489a921ce
Igor Kaseko <igorksk@gmail.com>
2025-08-09 18:03:31 +0300

    RotateMatrix

---
+++ 1.7.RotateMatrix/MatrixRotator.cs
@@ -0,0 +1,23 @@
+﻿namespace _1._7.RotateMatrix
+{
+    internal class MatrixRotator
+    {
+        public static int[,] RotateMatrix(int[,] matrix)
+        {
+            if (matrix == null || matrix.GetLength(0) != matrix.GetLength(1))
+            {
+                throw new ArgumentException("Input must be a square matrix.");
+            }
+            int n = matrix.GetLength(0);
+            int[,] rotatedMatrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    rotatedMatrix[j, n - 1 - i] = matrix[i, j];
+                }
+            }
+            return rotatedMatrix;
+        }
+    }
+}
---
+++ 1.7.RotateMatrix/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _1._7.RotateMatrix
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[,] matrix = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            Console.WriteLine("Original matrix:");
+            PrintMatrix(matrix);
+
+            int[,] rotated = MatrixRotator.RotateMatrix(matrix);
+
+            Console.WriteLine("\nRotated matrix:");
+            PrintMatrix(rotated);
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
