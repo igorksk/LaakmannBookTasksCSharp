c77289843c9b823d4640d778a4215a7225f95522
Igor Kaseko <igorksk@gmail.com>
2025-08-09 18:20:12 +0300

    IsRotation

---
+++ 1.9.IsRotation/Program.cs
@@ -0,0 +1,7 @@
+﻿using _1._9.IsRotation;
+
+string str1 = "waterbottle";
+string str2 = "erbottlewat";
+
+bool isRotation = RotationChecker.IsRotation(str1, str2);
+Console.WriteLine($"Is '{str2}' a rotation of '{str1}'? {isRotation}");
---
+++ 1.9.IsRotation/RotationChecker.cs
@@ -0,0 +1,24 @@
+﻿namespace _1._9.IsRotation
+{
+    internal class RotationChecker
+    {
+        // Helper method to check if str2 is a substring of str1
+        private static bool IsSubstring(string str1, string str2)
+        {
+            return str1.Contains(str2);
+        }
+
+        public static bool IsRotation(string str1, string str2)
+        {
+            // Check if lengths are equal
+            if (str1.Length != str2.Length)
+            {
+                return false;
+            }
+            // Concatenate str1 with itself
+            string concatenated = str1 + str1;
+            // Check if str2 is a substring of the concatenated string
+            return IsSubstring(concatenated, str2);
+        }
+    }
+}
