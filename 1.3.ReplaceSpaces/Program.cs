4e70d179b36dee3bc1d5686fbdf23120d1e6adf5
Igor Kaseko <igorksk@gmail.com>
2025-08-14 19:52:33 +0300

    small fix

--- 1.3.ReplaceSpaces/Program.cs
+++ 1.3.ReplaceSpaces/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            char[] input = {'M', 'r', ' ', 'J', 'o', 'h', 'n', ' ', 'S', 'm', 'i', 't', 'h', '\0', '\0', '\0', '\0'};
+            char[] input = ['M', 'r', ' ', 'J', 'o', 'h', 'n', ' ', 'S', 'm', 'i', 't', 'h', '\0', '\0', '\0', '\0'];
             int trueLength = 13;
 
             SpaceReplacer.ReplaceSpaces(input, trueLength);
